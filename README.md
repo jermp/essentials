e3ad764b80c7b271008350f9c77864b5ba3b4b68
Giulio Ermanno Pibiri <jeis90@gmail.com>
2019-02-02 20:31:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Essentials
+----------
+A header-only collection of some essential utilities that I use extensively for data structure design.
+Meant to be used in larger projects.
+
+See the `test/` folder for examples.
+To compile the examples, move into the folder and just
+type the following commands.
+
+	$ cmake .
+	$ make
---
+++ test/json_lines.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "../include/essentials.hpp"
+
+using namespace essentials;
+
+int main() {
+
+    json_lines jl;
+
+    jl.add("sum", "13");
+    double avg = 325.7;
+    jl.add("average", std::to_string(avg));
+
+    jl.new_line();
+    jl.add("foo", std::to_string(avg));
+    jl.add("bar", std::to_string(8894));
+    jl.add("baz", "value");
+
+    jl.print();
+
+    return 0;
+}
